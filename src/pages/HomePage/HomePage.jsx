4a95cfa41570808935ab18d717ebc497a9ad4ff5
Artem Matvieientsev <tmatveentsev@gmail.com>
2025-03-04 02:31:48 +0100

    text

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,10 @@
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      Hi this is my private pet-project, little phone book, you can register
+      here and create your personal contacts
+    </div>
+  );
 };
 
 export default HomePage;
